1ab8d2032343f7763011b7ee9c30d7bcfa3c0d9d
VITRUS-twt <contact@vitrushk.com>
2026-08-28 11:44:32 +0800

    Anthony commit

--- api/recommend.js
+++ api/recommend.js
@@ -45,8 +45,7 @@ Rules:
     const response = await fetch(endpoint, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            "x-goog-api-key": GEMINI_API_KEY
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(requestBody),
         signal: controller.signal
